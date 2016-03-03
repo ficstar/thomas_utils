195ea97ab596931e99cc57a599d048d567ef7b3a
Thomas Rogers <thomasrogers03@gmail.com>
2016-03-03 13:19:20 -0500

    Re-implement Future using Observation class

--- lib/thomas_utils/future.rb
+++ lib/thomas_utils/future.rb
@@ -1,42 +1,13 @@
 module ThomasUtils
-  class Future
+  class Future < Observation
     DEFAULT_EXECUTOR = ::Concurrent::CachedThreadPool.new
 
-    def initialize(options = {})
-      options[:executor] ||= DEFAULT_EXECUTOR
-
-      @mutex = Mutex.new
-      @future = ::Concurrent::Future.execute(executor: options[:executor]) do
-        begin
-          @result = yield
-          @result = @result.get if @result.is_a?(FutureWrapper)
-          @mutex.synchronize { @success_callback.call(@result) if @success_callback }
-          @result
-        rescue => e
-          @error = e
-          @mutex.synchronize { @failure_callback.call(e) if @failure_callback }
-        end
-      end
+    def initialize(options = {}, &block)
+      executor = options.fetch(:executor) { DEFAULT_EXECUTOR }
+      observable = Concurrent::Future.execute(executor: executor, &block)
+      super(executor, observable)
     end
 
-    def get
-      result = @future.value
-      raise @error if @error
-      result
-    end
-
-    def join
-      get rescue nil
-    end
-
-    def on_success(&block)
-      @mutex.synchronize { @success_callback = block }
-      @success_callback.call(@result) if @future.fulfilled? && !@error
-    end
-
-    def on_failure(&block)
-      @mutex.synchronize { @failure_callback = block }
-      @failure_callback.call(@error) if @future.fulfilled? && @error
-    end
   end
 end
+
--- spec/classes/future_spec.rb
+++ spec/classes/future_spec.rb
@@ -2,142 +2,60 @@ require 'rspec'
 
 module ThomasUtils
   describe Future do
-    let(:block) { good_block }
-    let(:good_block) { -> { 'Hello, World!' } }
-    let(:error_block) { -> { raise 'Goodbye, World!' } }
-    let(:fulfilled) { true }
-    let(:future_options) { {} }
-    let(:future) do
-      future = double(:future, fulfilled?: fulfilled)
-      allow(future).to receive(:value) { future_options[:value] }
-      future
+
+    let(:value) { Faker::Lorem.word }
+    let(:block_result) { [] }
+    let(:block) { ->() { block_result << value; value } }
+    let(:executor) { nil }
+    let(:result_executor) { executor || DEFAULT_EXECUTOR }
+    let(:options) do
+      {}.tap do |options|
+        options[:executor] = executor if executor
+      end
     end
-    let!(:result_id) { SecureRandom.uuid }
+    let(:future) { Future.new(options, &block) }
 
-    [nil, ::Concurrent::CachedThreadPool.new].each do |executor|
-      context "when the executor is #{executor || 'the default'}" do
-        let(:future_wrapper) { Future.new(executor: executor, &block) }
+    subject { future }
 
-        before do
-          expect(::Concurrent::Future).to receive(:execute).with(executor: executor || Future::DEFAULT_EXECUTOR) do |&block|
-            future_options[:value] = block.call
-          end.and_return(future)
+    before do
+      allow(Future::DEFAULT_EXECUTOR).to receive(:post) { |&block| block.call }
+    end
+
+    it { is_expected.to be_a_kind_of(Observation) }
+
+
+    describe 'execution' do
+      it { expect(Future::DEFAULT_EXECUTOR).to be_a_kind_of(Concurrent::CachedThreadPool) }
+
+      it 'should use execute within the default executor context' do
+        expect(Future::DEFAULT_EXECUTOR).to receive(:post) do |&block|
+          block.call
+          expect(block_result).to eq([value])
+        end
+        subject
+      end
+
+      it 'should support chained executions' do
+        salt = Faker::Lorem.word
+        expect(subject.then { |result| [result, salt] }.get).to eq([value, salt])
+      end
+
+      context 'with a specific executor' do
+        let(:executor) { Concurrent::ImmediateExecutor.new }
+
+        before { allow(Future::DEFAULT_EXECUTOR).to receive(:post) }
+
+        it 'should use execute within the default executor context' do
+          subject
+          expect(block_result).to eq([value])
         end
 
-        describe '#get' do
-          subject { future_wrapper.get }
-
-          it 'should be the value of the specified block' do
-            is_expected.to eq('Hello, World!')
-          end
-
-          context 'with a different value' do
-            let(:good_block) { -> { 'Goodbye, World!' } }
-
-            it 'should be the value of the specified block' do
-              is_expected.to eq('Goodbye, World!')
-            end
-          end
-
-          context 'when the block raises an error' do
-            let(:block) { error_block }
-
-            it 'should re-raise the error' do
-              expect { subject }.to raise_error('Goodbye, World!')
-            end
-          end
-
-          context 'when the result is a future wrapper' do
-            let(:result_future) { double(:future, get: 'Hello, World!') }
-            let(:result_future_wrapper) { FutureWrapper.new(result_future) { |res| res } }
-            let(:good_block) { -> { result_future_wrapper } }
-
-            it 'should return the result of that future' do
-              is_expected.to eq('Hello, World!')
-            end
-          end
-        end
-
-        describe '#join' do
-          subject { future_wrapper.join }
-
-          it 'should delegate to #get' do
-            expect(future_wrapper).to receive(:get)
-            subject
-          end
-
-          context 'when the block raises an error' do
-            let(:block) { error_block }
-
-            it 'should not raise an error' do
-              expect { subject }.not_to raise_error
-            end
-          end
-        end
-
-        describe '#on_success' do
-          it 'should yield the value' do
-            expect { |block| future_wrapper.on_success(&block) }.to yield_with_args('Hello, World!')
-          end
-
-          context 'when the block raises an error' do
-            let(:block) { error_block }
-
-            it 'should not yield' do
-              expect { |block| future_wrapper.on_success(&block) }.not_to yield_control
-            end
-          end
-
-          context 'when called after the block is already executed' do
-            before { future_wrapper.get rescue nil }
-
-            it 'should yield the value' do
-              expect { |block| future_wrapper.on_success(&block) }.to yield_with_args('Hello, World!')
-            end
-
-            context 'when the block raises an error' do
-              let(:block) { error_block }
-
-              it 'should not yield' do
-                expect { |block| future_wrapper.on_success(&block) }.not_to yield_control
-              end
-            end
-          end
-        end
-
-        describe '#on_failure' do
-          let(:error) { RuntimeError.new('Goodbye, World!') }
-          let(:error_block) { -> { raise error } }
-
-          it 'should not yield by default' do
-            expect { |block| future_wrapper.on_failure(&block) }.not_to yield_control
-          end
-
-          context 'when the block raises an error' do
-            let(:block) { error_block }
-
-            it 'should call the block with the provided error' do
-              expect { |block| future_wrapper.on_failure(&block) }.to yield_with_args(error)
-            end
-          end
-
-          context 'when called after the block is already executed' do
-            before { future_wrapper.get rescue nil }
-
-            it 'should not yield by default' do
-              expect { |block| future_wrapper.on_failure(&block) }.not_to yield_control
-            end
-
-            context 'when the block raises an error' do
-              let(:block) { error_block }
-
-              it 'should call the block with the provided error' do
-                expect { |block| future_wrapper.on_failure(&block) }.to yield_control
-              end
-            end
-          end
+        it 'should support chained executions' do
+          salt = Faker::Lorem.word
+          expect(subject.then { |result| [result, salt] }.get).to eq([value, salt])
         end
       end
     end
+
   end
 end
