b9792c3c4dd239f1e0199041f27a66472593e8ce
Thomas Rogers <thomasrogers03@gmail.com>
2016-09-02 08:19:54 -0400

    Fix namespace

--- lib/thomas_utils/observer.rb
+++ lib/thomas_utils/observer.rb
@@ -1,16 +1,18 @@
-module Observer
-  extend Forwardable
+module ThomasUtils
+  module Observer
+    extend Forwardable
 
-  def_delegators :@observation,
-                 :on_success,
-                 :on_failure,
-                 :on_complete,
-                 :on_timed,
-                 :join,
-                 :then,
-                 :none_fallback,
-                 :fallback,
-                 :ensure,
-                 :on_success_ensure,
-                 :on_failure_ensure
+    def_delegators :@observation,
+                   :on_success,
+                   :on_failure,
+                   :on_complete,
+                   :on_timed,
+                   :join,
+                   :then,
+                   :none_fallback,
+                   :fallback,
+                   :ensure,
+                   :on_success_ensure,
+                   :on_failure_ensure
+  end
 end
