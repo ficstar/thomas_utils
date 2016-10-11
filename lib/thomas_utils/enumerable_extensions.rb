1e52df7874fe48367b6b3c2f7a04877403968e7e
Thomas Rogers <thomasrogers03@gmail.com>
2016-10-11 17:44:29 -0400

    Implement Enumerable#lazy_filter enum modifier

--- lib/thomas_utils/enumerable_extensions.rb
+++ lib/thomas_utils/enumerable_extensions.rb
@@ -2,4 +2,9 @@ module Enumerable
   def lazy_limit(limit)
     ThomasUtils::Enum::Limiter.new(self, limit)
   end
+
+  def lazy_filter(filter = nil, &filter_block)
+    filter ||= filter_block
+    ThomasUtils::Enum::Filter.new(self, filter)
+  end
 end
--- spec/classes/enumerable_extensions_spec.rb
+++ spec/classes/enumerable_extensions_spec.rb
@@ -12,4 +12,20 @@ describe Enumerable do
     its(:to_a) { is_expected.to eq(enum[0...limit]) }
   end
 
+  describe '#lazy_filter' do
+    let(:limit) { rand(0...100) }
+    let(:filter) { ->(item) { item < limit } }
+    subject { enum.lazy_filter(&filter) }
+
+    it { is_expected.to be_a_kind_of(ThomasUtils::Enum::Filter) }
+    its(:to_a) { is_expected.to eq(enum.select(&filter)) }
+
+    context 'when passed in as a parameter instead of a block' do
+      subject { enum.lazy_filter(filter) }
+
+      it { is_expected.to be_a_kind_of(ThomasUtils::Enum::Filter) }
+      its(:to_a) { is_expected.to eq(enum.select(&filter)) }
+    end
+  end
+
 end
