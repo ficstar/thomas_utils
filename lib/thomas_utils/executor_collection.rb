435d50b194d2d5db758af920864f2565bafa6b3a
Thomas Rogers <thomasrogers03@gmail.com>
2016-10-14 17:39:58 -0400

    Implement ExecutorCollection stats

--- lib/thomas_utils/executor_collection.rb
+++ lib/thomas_utils/executor_collection.rb
@@ -12,6 +12,10 @@ module ThomasUtils
       @default_collection.build(name, max_threads, max_queue)
     end
 
+    def self.stats
+      @default_collection.stats
+    end
+
     def initialize
       @collection = {}
     end
@@ -24,6 +28,20 @@ module ThomasUtils
           fallback_policy: :caller_runs,
           auto_terminate: true
       )
+    end
+
+    def stats
+      @collection.inject({}) do |stats, (name, executor)|
+        executor_stats = {
+            maximum_active_tasks: executor.max_length,
+            maximum_queued_tasks: executor.max_queue,
+            largest_length: executor.largest_length,
+            completed: executor.completed_task_count,
+            pending: executor.queue_length,
+            active: executor.scheduled_task_count - executor.completed_task_count - executor.queue_length
+        }
+        stats.merge(name => executor_stats)
+      end
     end
 
     @default_collection = new.freeze
--- spec/classes/executor_collection_spec.rb
+++ spec/classes/executor_collection_spec.rb
@@ -22,6 +22,33 @@ module ThomasUtils
         its(:fallback_policy) { is_expected.to eq(:caller_runs) }
         its(:auto_terminate?) { is_expected.to eq(true) }
       end
+
+      describe '#stats' do
+        let(:name) { Faker::Lorem.sentence }
+        let(:max_threads) { rand(1..100) }
+        let(:max_queue) { rand(100..5000) }
+        let(:largest_length) { rand(1..max_threads) }
+        let(:scheduled_task_count) { rand(1..max_queue) }
+        let(:completed_task_count) { rand(1..scheduled_task_count) }
+        let(:queue_length) { rand(1..completed_task_count) }
+
+        before do
+          collection.build(name, max_threads, max_queue)
+          allow_any_instance_of(Concurrent::ThreadPoolExecutor).to receive(:largest_length).and_return(largest_length)
+          allow_any_instance_of(Concurrent::ThreadPoolExecutor).to receive(:scheduled_task_count).and_return(scheduled_task_count)
+          allow_any_instance_of(Concurrent::ThreadPoolExecutor).to receive(:completed_task_count).and_return(completed_task_count)
+          allow_any_instance_of(Concurrent::ThreadPoolExecutor).to receive(:queue_length).and_return(queue_length)
+        end
+
+        subject { collection.stats[name] }
+
+        it { is_expected.to include(maximum_active_tasks: max_threads) }
+        it { is_expected.to include(maximum_queued_tasks: max_queue) }
+        it { is_expected.to include(largest_length: largest_length) }
+        it { is_expected.to include(completed: completed_task_count) }
+        it { is_expected.to include(pending: queue_length) }
+        it { is_expected.to include(active: scheduled_task_count - completed_task_count - queue_length) }
+      end
     end
 
     it_behaves_like 'an executor collection'
